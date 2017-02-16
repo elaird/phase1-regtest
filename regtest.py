ee8517b955fad35ab815c7ab34a0dbe85fd302a5
Joe Pastika <joe.pastika@gmail.com>
2017-02-16 16:16:52 +0100

    Add mature implementation of pexpect interface

--- regtest.py
+++ regtest.py
@@ -1,29 +1,11 @@
 #!/usr/bin/env python
 
 import registers
-import os, pexpect, re
 
-
-def responses(connection="", cmds=""):
-    out = []
-    p = pexpect.spawn(connection)
-    p.sendline("")
-    p.expect(".*")
-
-    for cmd in cmds:
-        p.sendline(cmd)
-        p.expect("{0}\s?#((\s|E)[^\r^\n]*)".format(re.escape(cmd)), timeout=5)
-        out.append(p.match.group(0))
-        print out[-1]
-
-    p.sendline("quit")
-    p.expect(pexpect.EOF)
-    p.close()
-    # os.system("killall ngccm >& /dev/null")
-    return out
-
+from ngFECSendCommand import send_commands
 
 if __name__ == "__main__":
-    connection = "ngFEC.exe -z -c -p 64004 -H hcal904daq04"
+    port = 64004
+    host = hcal904daq04
     cmds = ["get HE15-1-1-i_%s" % x for x in registers.i_readables()]
-    results = responses(connection, cmds)
+    results = send_commands(port, host, cmds, script=True, raw=False, timeout=20)
