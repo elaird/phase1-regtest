81188ed30996d40ae7f8a52ae338cd2f393bca2b
Edward Laird <elaird@gmail.com>
2017-12-15 20:11:42 +0100

    add configuration options and two monitorables

--- powerMon.py
+++ powerMon.py
@@ -33,7 +33,13 @@ def opts():
     parser.add_option("--log-file",
                       dest="logfile",
                       default="/nfshome0/elaird/powerMon.log")
-
+    parser.add_option("--rbxes",
+                      dest="rbxes",
+                      default="HE1,HE2,HE3,HE4,HE5,HE6,HE7,HE8,HE9,HE10,HE11,HE12,HE13,HE14,HE15,HE16,HE17,HE18")
+    parser.add_option("--no-uhtr",
+                      dest="noUhtr",
+                      default=False,
+                      action="store_true")
     return parser.parse_args()
 
 
@@ -47,7 +53,7 @@ def main(options, _):
     logfile.write("\n" + str(datetime.datetime.today()))
 
     server = ngfec.connect(options.host, options.port) #, ccmLogfile)
-    work(server, logfile)
+    work(server, logfile, options.rbxes.split(","), not options.noUhtr)
     ngfec.disconnect(server)
 
     # ccmLogfile.close()
@@ -55,20 +61,20 @@ def main(options, _):
     logfile.close()
 
 
-def work(server, logfile):
-    for iBox in range(1, 19):
-        rbx = "HE%d" % iBox
+def work(server, logfile, rbxes, uhtr):
+    for rbx in rbxes:
         for card in ["J14", "J15"]:
-            for var in ["vtrx_rssi", "3V3_bkp", "1V2_voltage", "1V2_current"]:
+            for var in ["vtrx_rssi", "3V3_bkp", "1V2_voltage", "1V2_current", "2V5_voltage", "VIN_voltage"]:
                 logfile.write("\n" + ngfec.command(server, "get %s-%s_%s_Cntrl_f_rr" % (rbx, var, card))[0])
 
         logfile.write("\n" + ngfec.command(server, "get %s-fec-sfp_tx_power_f" % rbx)[0])
         logfile.write("\n" + ngfec.command(server, "get %s-fec-sfp_rx_power_f" % rbx)[0])
 
-    for iPod in range(2):
-        uhtr_powers = commandOutput("uHTRtool.exe -c 63:7 -s linkStatus.uhtr | grep PPOD%d -A 1 | tail -1" % iPod)
-        s = " ".join(uhtr_powers).replace(" (uW)", "%d #" % iPod)
-        logfile.write("\nget " + s)
+    if uhtr:
+        for iPod in range(2):
+            uhtr_powers = commandOutput("uHTRtool.exe -c 63:7 -s linkStatus.uhtr | grep PPOD%d -A 1 | tail -1" % iPod)
+            s = " ".join(uhtr_powers).replace(" (uW)", "%d #" % iPod)
+            logfile.write("\nget " + s)
 
 
 if __name__ == "__main__":
